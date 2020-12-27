832cf99e88a879d814074144cf6985ad6b77f196
Sumit134coder <73213318+Sumit134coder@users.noreply.github.com>
2020-12-27 18:16:09 +0530

    chore
    intitialized

---
+++ app.js
@@ -0,0 +1,32 @@
+var userInput = document.querySelector("#input")
+var btn = document.querySelector("#submit")
+var luckyNumber = document.querySelector("#luckyNumber")
+var output = document.querySelector(".output")
+
+var total= 0;
+var Sum=0;
+function btnHandler(){
+    console.log("clicked")
+    var input = userInput.value;
+    for(var i=0;i<input.length;i++){
+        if(input[i]!='-')
+        Sum += parseInt(input[i]);
+    }
+    console.log(Sum);
+
+    if(Sum>=luckyNumber.value)
+    {
+    var diff = Sum-luckyNumber.value;
+    }
+    else
+    diff=luckyNumber.value-Sum;
+console.log("differnce is"+ diff)
+alert("you are " + diff + " days far fromm being lucky")
+output.innerText = "you are  days far from being lucky";
+
+
+}
+
+
+btn.addEventListener("click",btnHandler);
+
