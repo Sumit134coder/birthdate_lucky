ecfe2eb7a6109d2d1ea63c34036b651eb6858d5a
Sumit134coder <73213318+Sumit134coder@users.noreply.github.com>
2021-01-01 14:14:36 +0530

    feat: gif added

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var userInput = document.querySelector("#input")
 var btn = document.querySelector("#submit")
 var luckyNumber = document.querySelector("#luckyNumber")
 var output = document.querySelector("#output")
+var image = document.querySelector("iframe")
 
 var Sum=0;
 function btnHandler(){
@@ -15,9 +16,12 @@ function btnHandler(){
     console.log(Sum%luckyNumber.value);
     if(Sum%luckyNumber.value==0||luckyNumber.value%Sum==0)
     {
+        image.src = "https://giphy.com/embed/5C2aHYut0kzXrf1Z7D";
         output.innerHTML = "you are lucky";
+
     }
     else
+    image.src = "https://giphy.com/embed/qBVEww0YjwWyI";
     output.innerHTML = "unlucky";
 }
 
