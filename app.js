917c28b4df1fce7561f82735960d84df2da04fc5
Sumit134coder <73213318+Sumit134coder@users.noreply.github.com>
2021-08-02 13:35:23 +0530

    refactor

--- app.js
+++ app.js
@@ -1,31 +1,30 @@
-var userInput = document.querySelector("#input")
-var btn = document.querySelector("#submit")
-var luckyNumber = document.querySelector("#luckyNumber")
-var output = document.querySelector("#output")
-var image = document.querySelector("iframe")
-
-var Sum=0;
-function btnHandler(){
-    alert("as a website, your data will not be stored");
-    console.log("clicked")
-    var input = userInput.value;
-    for(var i=0;i<input.length;i++){
-        if(input[i]!='-')
-        Sum += parseInt(input[i]);
+const luckyNumber = document.querySelector("#luckyNumber")
+const birthdayDate = document.querySelector("#date-of-birth")
+const Submit = document.querySelector("button")
+const giphy = document.querySelector(".giphy-embed")
+const outputBody = document.querySelector(".output-body")
+Submit.addEventListener("click", GetData)
+window.confirm("This site will not store your data")
+function GetData(){
+    giphy.style.visibility = "visible"
+    console.log(birthdayDate.value)
+    console.log(birthdayDate.value.replaceAll("-","0"))
+    let dateString = birthdayDate.value.replaceAll("-","0");
+    let sum=0;
+    for(let index = 0 ; index < dateString.length ; index++){
+        sum += Number(dateString[index])
     }
-    console.log(Sum%luckyNumber.value);
-    if(Sum%luckyNumber.value==0||luckyNumber.value%Sum==0)
-    {
-        image.src = "https://giphy.com/embed/5C2aHYut0kzXrf1Z7D";
-        output.innerHTML = "you are lucky";
-
+    console.log(sum);
+    if(sum%Number(luckyNumber.value)==0){
+        console.log("lucky")
+        outputBody.innerText = "You are so lucky"
+        giphy.src = "https://giphy.com/embed/5C2aHYut0kzXrf1Z7D"
     }
     else{
-    image.src = "https://giphy.com/embed/qBVEww0YjwWyI";
-    output.innerHTML = "unlucky";}
+        console.log("unlucky")
+        outputBody.innerText = "Not so lucky"
+        giphy.src = "https://giphy.com/embed/qBVEww0YjwWyI"
+    }
+
 }
-
-
-
-btn.addEventListener("click",btnHandler);
-
+//<iframe src="https://giphy.com/embed/qBVEww0YjwWyI" width="480" height="270" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/league-of-legends-esports-caster-jun-qBVEww0YjwWyI">via GIPHY</a></p>
